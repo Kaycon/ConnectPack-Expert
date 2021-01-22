4dd6ffc810b5630fe087fde0d5625c92ff44b291
Jona Ostermann <Jona-Ostermann@web.de>
2021-01-23 00:42:40 +0100

    Added kubejs

---
+++ kubejs/server_scripts/settings.js
@@ -0,0 +1,8 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('KubeJS: Set settings')
