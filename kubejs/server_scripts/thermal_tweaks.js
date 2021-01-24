28b5531b882c9fa3c5a24b4ad33b0e4c3a54ff84
Jona Ostermann <Jona-Ostermann@web.de>
2021-01-24 20:21:32 +0100

    Removed crafting table gears recipes and added gearworking die recipes for lapis, diamond, emerald and quartz gear

--- kubejs/server_scripts/thermal_tweaks.js
+++ kubejs/server_scripts/thermal_tweaks.js
@@ -5,4 +5,43 @@ onEvent('recipes', event => {
   
   // Remove Crafting Table Bronze Dust recipe
   event.remove({id: 'thermal:bronze_dust_4'})
+
+  
+  ///////////////////////////////////////////
+  // Remove Crafting Table Gears Recipes
+  ///////////////////////////////////////////
+
+  var thermal_gears = [
+    'iron_gear',
+    'gold_gear',
+    'lapis_gear',
+    'diamond_gear',
+    'emerald_gear',
+    'quartz_gear',
+    'copper_gear',
+    'tin_gear',
+    'lead_gear',
+    'silver_gear',
+    'nickel_gear',
+    'bronze_gear',
+    'electrum_gear',
+    'invar_gear',
+    'constantan_gear',
+    'signalum_gear',
+    'lumium_gear',
+    'enderium_gear'
+
+  ]
+  for (i = 0; i < thermal_gears.length; i++) {
+    event.remove({id: 'thermal:parts/'+thermal_gears[i]})
+  } 
+
+  event.recipes.thermal.press('thermal:lapis_gear', [item.of('minecraft:lapis_lazuli', 4), 'thermal:press_gear_die'])
+  event.recipes.thermal.press('thermal:diamond_gear', [item.of('minecraft:diamond', 4), 'thermal:press_gear_die'])
+  event.recipes.thermal.press('thermal:emerald_gear', [item.of('minecraft:emerald', 4), 'thermal:press_gear_die'])
+  event.recipes.thermal.press('thermal:quartz_gear', [item.of('minecraft:quartz', 4), 'thermal:press_gear_die'])
+
+
+  
+
 })
