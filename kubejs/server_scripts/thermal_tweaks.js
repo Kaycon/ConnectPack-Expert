2ced86bbe344d407125de8a81deb281559f8c1ec
Jona Ostermann <Jona-Ostermann@web.de>
2021-01-24 22:13:22 +0100

    Added more thermal gears, machine frame and machine press tweaks

--- kubejs/server_scripts/thermal_tweaks.js
+++ kubejs/server_scripts/thermal_tweaks.js
@@ -7,41 +7,84 @@ onEvent('recipes', event => {
   event.remove({id: 'thermal:bronze_dust_4'})
 
   
-  ///////////////////////////////////////////
-  // Remove Crafting Table Gears Recipes
-  ///////////////////////////////////////////
+  ////////////////////////////////////////////////////////
+  // Remove Crafting Table and Metal Press Gears Recipes
+  ////////////////////////////////////////////////////////
 
   var thermal_gears = [
-    'iron_gear',
-    'gold_gear',
-    'lapis_gear',
-    'diamond_gear',
-    'emerald_gear',
-    'quartz_gear',
-    'copper_gear',
-    'tin_gear',
-    'lead_gear',
-    'silver_gear',
-    'nickel_gear',
-    'bronze_gear',
-    'electrum_gear',
-    'invar_gear',
-    'constantan_gear',
-    'signalum_gear',
-    'lumium_gear',
-    'enderium_gear'
+    'iron',
+    'gold',
+    'lapis',
+    'diamond',
+    'emerald',
+    'quartz',
+    'copper',
+    'tin',
+    'lead',
+    'silver',
+    'nickel',
+    'bronze',
+    'electrum',
+    'invar',
+    'constantan',
+    'signalum',
+    'lumium',
+    'enderium'
 
   ]
   for (i = 0; i < thermal_gears.length; i++) {
-    event.remove({id: 'thermal:parts/'+thermal_gears[i]})
+    event.remove({id: 'thermal:parts/'+thermal_gears[i]+'_gear'})
+    event.remove({id: 'immersiveengineering:metalpress/gear_'+thermal_gears[i]})
   } 
 
+  // Gearworking Die
+  event.remove({id: 'thermal:press_gear_die'})
+  event.shaped('thermal:press_gear_die', [
+    'DID',
+    'IDI',
+    'DID'
+  ], {
+    I: 'thermal:invar_plate',
+    D: 'minecraft:diamond'
+  })
+
+  // Add Vanilla materials gears to press
   event.recipes.thermal.press('thermal:lapis_gear', [item.of('minecraft:lapis_lazuli', 4), 'thermal:press_gear_die'])
   event.recipes.thermal.press('thermal:diamond_gear', [item.of('minecraft:diamond', 4), 'thermal:press_gear_die'])
   event.recipes.thermal.press('thermal:emerald_gear', [item.of('minecraft:emerald', 4), 'thermal:press_gear_die'])
   event.recipes.thermal.press('thermal:quartz_gear', [item.of('minecraft:quartz', 4), 'thermal:press_gear_die'])
 
+  ///////////////////////////////////////////
+  // Thermal Crafting Parts
+  ///////////////////////////////////////////
 
+  // Machine Frame
+  event.remove({id: 'thermal:machine_frame'})
+  event.shaped('thermal:machine_frame', [
+    'IGI',
+    'GSG',
+    'IGI'
+  ], {
+    I: 'thermal:invar_ingot',
+    G: '#forge:glass',
+    S: 'mekanism:steel_casing'
+  })
+
+  ///////////////////////////////////////////
+  // Thermal Machines
+  ///////////////////////////////////////////
   
-
+  // Machine Frame
+  event.remove({id: 'thermal:machine_press'})
+  event.shaped('thermal:machine_press', [
+    ' P ',
+    'BFB',
+    'RPR'
+  ], {
+    P: 'minecraft:piston',
+    B: 'mekanism:ingot_bronze',
+    F: 'thermal:machine_frame',
+    R: 'thermal:rf_coil',
+    P: 'minecraft:piston'
+  })
 })
