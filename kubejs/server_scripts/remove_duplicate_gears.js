858f3394d3b4da363164da60199dd58db25c735f
Jona Ostermann <Jona-Ostermann@web.de>
2021-01-24 19:52:43 +0100

    Remove duplicate gears

---
+++ kubejs/server_scripts/remove_duplicate_gears.js
@@ -0,0 +1,5 @@
+onEvent('recipes', event => {
+  event.replaceInput({}, 'titanium:gold_gear', 'thermal:gold_gear')
+  event.replaceInput({}, 'titanium:iron_gear', 'thermal:iron_gear')
+  event.replaceInput({}, 'titanium:diamond_gear', 'thermal:diamond_gear')
+})
