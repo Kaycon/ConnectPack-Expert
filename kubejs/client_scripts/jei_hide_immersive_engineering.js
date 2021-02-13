fffb2d060796b42b8fa7287304249c83646a914c
Jona Ostermann <Jona-Ostermann@web.de>
2021-02-13 15:35:26 +0100

    Removed Immersive Engineering Steel Armor

---
+++ kubejs/client_scripts/jei_hide_immersive_engineering.js
@@ -0,0 +1,6 @@
+onEvent('jei.hide.items', event => {
+  event.hide('immersiveengineering:armor_steel_feet')
+  event.hide('immersiveengineering:armor_steel_legs')
+  event.hide('immersiveengineering:armor_steel_chest')
+  event.hide('immersiveengineering:armor_steel_head')
+})
--- kubejs/server_scripts/immersive_engineering_tweaks.js
+++ kubejs/server_scripts/immersive_engineering_tweaks.js
@@ -3,6 +3,12 @@ onEvent('recipes', event => {
   event.remove({id: 'immersiveengineering:crafting/plate_steel_hammering'})
   event.recipes.thermal.press('immersiveengineering:plate_steel', ['mekanism:ingot_steel'])
 
+  // Remove Immersive Steel Armor
+  event.remove({id: 'immersiveengineering:crafting/armor_steel_feet'})
+  event.remove({id: 'immersiveengineering:crafting/armor_steel_legs'})
+  event.remove({id: 'immersiveengineering:crafting/armor_steel_chest'})
+  event.remove({id: 'immersiveengineering:crafting/armor_steel_head'})
+
   // Create Mekanism Steel with Immersive Engineering Blast Furnace
   //event.remove({id: 'immersiveengineering:blastfurnace/steel'})
   //event.recipes.immersiveengineering.blast_furnace('mekanism:ingot_steel', 'minecraft:iron_ingot', 'immersiveengineering:slag')
