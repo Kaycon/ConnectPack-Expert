00f1b425f32a9123ec4f60cb5e2c1d45f71652f6
Jona Ostermann <Jona-Ostermann@web.de>
2021-02-13 20:10:49 +0100

    Hid Avaritia Singularities

---
+++ kubejs/client_scripts/jei_hide_avaritia.js
@@ -0,0 +1,9 @@
+onEvent('jei.hide.items', event => {
+  event.hide('avaritia:iron_singularity')
+  event.hide('avaritia:golden_singularity')
+  event.hide('avaritia:lapis_singularity')
+  event.hide('avaritia:redstone_singularity')
+  event.hide('avaritia:nether_quartz_singularity')
+  event.hide('avaritia:diamond_singularity')
+  event.hide('avaritia:emerald_singularity')
+})
