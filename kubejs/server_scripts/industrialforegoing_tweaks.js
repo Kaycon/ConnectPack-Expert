458b686e7715882e99140b96fa5cd0eae64d09e0
Jona Ostermann <Jona-Ostermann@web.de>
2021-02-16 18:17:09 +0100

    Tweaked Simple Machine Frame

--- kubejs/server_scripts/industrialforegoing_tweaks.js
+++ kubejs/server_scripts/industrialforegoing_tweaks.js
@@ -58,6 +58,43 @@ event.shaped('industrialforegoing:machine_frame_pity', [
   E: 'thermal:electrum_gear'
 })
 
+event.remove({id:'industrialforegoing:dissolution_chamber/simple_machine_frame'})
+event.custom({
+  "input": [
+    {
+      "tag": "forge:plastic"
+    },
+    {
+      "tag": "industrialforegoing:machine_frame/pity"
+    },
+    {
+      "tag": "forge:plastic"
+    },
+    {
+      "item": "thermal:lumium_gear"
+    },
+    {
+      "item": "thermal:lumium_gear"
+    },
+    {
+      "item": "thermal:enderium_plate"
+    },
+    {
+      "tag": "forge:gears/gold"
+    },
+    {
+      "item": "thermal:enderium_plate"
+    }
+  ],
+  "inputFluid": "{FluidName:\"industrialforegoing:latex\",Amount:250}",
+  "processingTime": 300,
+  "output": {
+    "item": "industrialforegoing:machine_frame_simple",
+    "count": 1
+  },
+  "type": "industrialforegoing:dissolution_chamber"
+})
+
 event.remove({id:'industrialforegoing:dissolution_chamber/advanced_machine_frame'})
 event.custom({
   "input": [
