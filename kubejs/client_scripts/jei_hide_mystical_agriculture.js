96eae9490b9972ef8bd2567f0c010239fcc1f3b8
Jona Ostermann <Jona-Ostermann@web.de>
2021-02-19 16:57:52 +0100

    Removed Infusion Altar and Infusion Pedestal recipes and JEI

---
+++ kubejs/client_scripts/jei_hide_mystical_agriculture.js
@@ -0,0 +1,4 @@
+onEvent('jei.hide.items', event => {
+  event.hide('mysticalagriculture:infusion_altar')
+  event.hide('mysticalagriculture:infusion_pedestal')
+})
---
+++ kubejs/server_scripts/mystical_agriculture.js
@@ -0,0 +1,6 @@
+onEvent('recipes', event => {
+  
+  event.remove({id: 'mysticalagriculture:infusion_altar'})
+  event.remove({id: 'mysticalagriculture:infusion_pedestal'})
+
+})
