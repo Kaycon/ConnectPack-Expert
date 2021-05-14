a89751b91fb19b8e1213f9402c8efe00256c674a
Jona Ostermann <Jona-Ostermann@web.de>
2021-05-14 15:08:51 +0200

    hide uraninite seeds and essence from jei

--- kubejs/client_scripts/jei_hide_mystical_agriculture.js
+++ kubejs/client_scripts/jei_hide_mystical_agriculture.js
@@ -1,4 +1,6 @@
 onEvent('jei.hide.items', event => {
   event.hide('mysticalagriculture:infusion_altar')
   event.hide('mysticalagriculture:infusion_pedestal')
+  event.hide('mysticalagriculture:uraninite_seeds')
+  event.hide('mysticalagriculture:uraninite_essence')
 })
