e37e921008bfc9fb279a768746573fdd34d453ac
Jona Ostermann <Jona-Ostermann@web.de>
2021-05-14 15:11:13 +0200

    tweaked azure electrum recipe

--- kubejs/server_scripts/silentgear_tweaks.js
+++ kubejs/server_scripts/silentgear_tweaks.js
@@ -8,4 +8,12 @@ onEvent('recipes', event => {
   event.recipes.thermal.pulverizer(Item.of('silentgear:crimson_iron_dust', 2), ['silentgear:crimson_iron_ore'])
   event.recipes.mekanism.enriching(Item.of('silentgear:crimson_iron_dust', 2), ['silentgear:crimson_iron_ore'])
 
+  // Azure Electrum
+  event.remove({id: 'silentgear:azure_electrum_ingot'})
+  event.recipes.thermal.smelter('silentgear:azure_electrum_ingot', [Item.of('thermal:enderium_ingot', 2), Item.of('thermal:electrum_ingot', 4), Item.of('silentgear:azure_silver_ingot', 4)])
+  
+  // Azure Silver Dust
+  event.recipes.thermal.pulverizer(Item.of('silentgear:azure_silver_dust', 2), ['silentgear:azure_silver_ore'])
+  event.recipes.mekanism.enriching(Item.of('silentgear:azure_silver_dust', 2), ['silentgear:azure_silver_ore'])
+
 })
